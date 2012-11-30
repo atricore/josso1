8daa033f84891cbb8dbabc38f8ce4b75b1f45512
Gianluca Brigandi <gbrigandi@atricore.org>
2012-11-30 09:52:52 -0500

    First commit of GateIn Agent for JOSSO

---
+++ agents/josso-gatein-agent/josso-gatein-agent-main/src/main/java/org/josso/gatein/agent/GateInSSOAgentFilter.java
@@ -0,0 +1,97 @@
+/*
+ * JBoss, a division of Red Hat
+ * Copyright 2006, Red Hat Middleware, LLC, and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.josso.gatein.agent;
+
+import org.josso.servlet.agent.GenericServletSSOAgentFilter;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Heavily based on Gatein's logout filter class.
+ *
+ * @author <a href="mailto:gbrigandi@atricore.org">Gianluca Brigandi</a>
+ */
+public class GateInSSOAgentFilter extends GenericServletSSOAgentFilter
+{
+    protected String logoutUrl;
+    private static final String fileEncoding = System.getProperty("file.encoding");
+
+    public void init(FilterConfig config) throws ServletException
+    {
+        super.init(config);
+        this.logoutUrl = config.getInitParameter("logoutUrl");
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException
+    {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        boolean isLogoutInProgress = this.isLogoutInProgress(httpRequest);
+
+        if (isLogoutInProgress)
+        {
+
+            if (httpRequest.getSession().getAttribute("SSO_LOGOUT_FLAG") == null)
+            {
+                httpRequest.getSession().setAttribute("SSO_LOGOUT_FLAG", Boolean.TRUE);
+
+                httpResponse.sendRedirect(logoutUrl);
+                return;
+            }
+            else
+            {
+                // clear the LOGOUT flag
+                httpRequest.getSession().removeAttribute("SSO_LOGOUT_FLAG");
+            }
+        }
+
+        super.doFilter(request, response, chain);
+    }
+
+    private boolean isLogoutInProgress(HttpServletRequest request) throws UnsupportedEncodingException
+    {
+        // set character encoding before retrieving request parameters
+        if(fileEncoding!=null)
+        {
+            request.setCharacterEncoding(fileEncoding);
+        }
+        String action = request.getParameter("portal:action");
+
+        if (action != null && action.equals("Logout"))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+}
---
+++ agents/josso-gatein-agent/josso-gatein-agent-main/src/main/java/org/josso/gatein/agent/GateinSSOAgent.java
@@ -0,0 +1,81 @@
+/*
+ * JOSSO: Java Open Single Sign-On
+ *
+ * Copyright 2004-2009, Atricore, Inc.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ */
+
+package org.josso.gatein.agent;
+
+import org.josso.agent.SSOAgentRequest;
+import org.josso.agent.SingleSignOnEntry;
+import org.josso.servlet.agent.GenericServletLocalSession;
+import org.josso.servlet.agent.GenericServletSSOAgent;
+import org.josso.servlet.agent.GenericServletSSOAgentRequest;
+import org.gatein.wci.security.Credentials;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @org.apache.xbean.XBean element="agent"
+ *
+ * @author <a href="mailto:gbrigand@josso.org">Gianluca Brigandi</a>
+ *
+ */
+public class GateinSSOAgent extends GenericServletSSOAgent {
+
+    private static final Log log = LogFactory.getLog(GateinSSOAgent.class);
+
+    /**
+     * This extension will delegate processing to super class and publish JOSSO Security Context
+     * instance in the LocalSession associated to the request.
+     */
+    protected SingleSignOnEntry execute(SSOAgentRequest request) {
+        GenericServletSSOAgentRequest r = (GenericServletSSOAgentRequest) request;
+        GenericServletLocalSession localSession = (GenericServletLocalSession) r.getLocalSession();
+
+        SingleSignOnEntry  entry = super.execute(request);
+
+        if (entry != null) {
+            if (r.getSecurityContext() != null) {
+                String principal = r.getSecurityContext().getCurrentPrincipal().getName();
+
+                log.info("-----------------------------------------------------------");
+                log.info("SessionId: "+entry.ssoId);
+                log.info("Principal: "+principal);
+                log.info("-----------------------------------------------------------");
+
+                Credentials credentials = new Credentials(principal, "");
+                localSession.setAttribute(Credentials.CREDENTIALS, credentials);
+                localSession.setAttribute("username", principal);
+            }
+
+        } else {
+            if (localSession != null) {
+                if (log.isDebugEnabled())
+                    log.debug("Clearing JOSSO Security Context for session ["+ localSession.getId() +  "]");
+
+                localSession.setSecurityContext(null);
+                r.setSecurityContext(null);
+            }
+        }
+
+        return entry;
+    }
+
+}

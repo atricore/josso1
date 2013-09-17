42ae0090a8a76a85d039c13ff2aff3d5d206ffd1
sgonzalez <sgonzalez@josso.org>
2013-09-17 09:45:03 -0400

    * Updated ISAPI Agent version to 1.8.9

--- agents/josso-isapi-agent/JOSSOC/src/JOSSOIsapiAgent/isapi/josso_isapi.h
+++ agents/josso-isapi-agent/JOSSOC/src/JOSSOIsapiAgent/isapi/josso_isapi.h
@@ -27,7 +27,7 @@
 #define JOSSO_VERMAJOR     1
 #define JOSSO_VERMINOR     8
 #define JOSSO_VERFIX       4
-#define JOSSO_VERSTRING    "1.8.8"
+#define JOSSO_VERSTRING    "1.8.9"
 
 /* set JOSSO_VERISRELEASE to 1 when release (do not forget to commit!) */
 #define JOSSO_VERISRELEASE 1
